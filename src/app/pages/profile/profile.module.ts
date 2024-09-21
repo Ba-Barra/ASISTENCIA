79f34b7d5e78f48453d21778d9c60510fe1192be
Bayron <ba.barra@duocuc.cl>
2024-09-20 21:23:38 -0300

    perfil form

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePageRoutingModule } from './profile-routing.module';
-
 import { ProfilePage } from './profile.page';
 
 @NgModule({
@@ -13,7 +11,8 @@ import { ProfilePage } from './profile.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    ReactiveFormsModule // Añadir ReactiveFormsModule aquí
   ],
   declarations: [ProfilePage]
 })
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { 
+    // Inicializar el formulario aquí para evitar el error
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  ngOnInit() {
+    // El formulario ya está inicializado en el constructor, así que este método puede estar vacío
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Login exitoso con usuario:', username, 'y contraseña:', password);
+      // Aquí podrías agregar la lógica para manejar el inicio de sesión
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
